d6a98817497592b372e2bc830f819cba5bbe7cbb
paulo monter <paul@paul.com>
2016-12-03 19:08:53 +0100

    Test funkcjonalny teraz sprawdza mozliwosc dodania elementu listy.

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -15,6 +16,28 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.get('http://localhost:8000')
 
 		self.assertIn('Listy',self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('Listy', header_text)
+
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+		inputbox.get_attribute('placeholder'),
+		'Wpisz rzecz do zrobienia'
+		)
+
+		inputbox.send_keys('Kupic pawie piora')
+
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_element_by_tag_name('tr')
+
+		self.assertTrue(
+		any(row.text == '1: Kupic pawie piora' for row in rows)
+
+		)
+
+
 		self.fail('Zakonczenie testu!')
 
 if __name__=='__main__':
