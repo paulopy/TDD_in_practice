1b12e2a082563bcbf58385792ed9a83a156917f8
paulo monter <paul@paul.com>
2016-12-08 09:24:06 +0100

    bakup

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,11 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
-from lists.views import home_page
+
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
+from lists.views import home_page
+from lists.models import Item
 
 class HomePageTest(TestCase):
 	def test_root_url_resolves_to_home_page_view(self):
@@ -22,10 +24,40 @@ class HomePageTest(TestCase):
 		request.POST['item_text'] = 'Nowy element listy'
 
 		response = home_page(request)
+
+		self.assertEqual(Item.objects.count(),1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text,'Nowy element listy')
+
 		self.assertIn('Nowy element listy',response.content.decode())
 		expected_html = render_to_string(
 		'home.html',
 		{'new_item_text':'Nowy element listy'}
 		)
 		self.assertEqual(response.content.decode(),expected_html)
-		
+
+
+	def test_home_page_only_saves_items_when_necessary(self):
+		request = HttpRequest()
+		home_page(request)
+		self.assertEqual(Item.objects.count(),0)
+
+
+class ItemModelTest(TestCase):
+	def test_saving_and_retrevieng_items(self):
+		first_item = Item()
+		first_item.text = 'Absolutnie pierwszy element listy'
+		first_item.save()
+
+		second_item = Item()
+		second_item.text = 'Drugi element'
+		second_item.save()
+
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(),2)
+
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+
+		self.assertEqual(first_saved_item.text, 'Absolutnie pierwszy element listy')
+		self.assertEqual(second_saved_item.text, 'Drugi element')
